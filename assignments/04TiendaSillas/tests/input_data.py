c40323be16bafe88297e53c821392c16612e4850
Ma. Guadalupe Roque <roque@itesm.mx>
2023-04-16 19:31:49 -0600

    ACTUALIZADO

--- assignments/04TiendaSillas/tests/input_data.py
+++ assignments/04TiendaSillas/tests/input_data.py
@@ -8,148 +8,46 @@
 input_values = [
     # Test case 1
     (
-        ["iii9", "F", "10"],
-        ["Tipo de laptop i5, i7, i9: ",
-         "Tipo de cliente F, R, N: ",
-          "Cantidad de laptops: ",
-        "Error en tipo de laptop"],
+        ["1",
+         "0","0","3","0"],
+        ["[['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]",
+         "Jugador1",
+        "r1:",
+        "c1:",
+        "r2:",
+        "c2:",
+        "['a', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['a', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]",
+        "Par",
+        "[['a', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['a', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]",
+        "Pares Jugador1: 1"
+         ],
         '''Debe salir:
-        Error en tipo de laptop
+        otra cosa
         '''
     ),
     (
-        ["i9", "Tec", "10"],
-        ["Tipo de laptop i5, i7, i9: ",
-         "Tipo de cliente F, R, N: ",
-          "Cantidad de laptops: ",
-        "Error en tipo de cliente"],
+        ["1",
+         "0","0","3","0",
+        "0","1","3","1",
+        "0","2","3","2",
+        "0","3","3","3",
+        "0","4","3","4",
+        "0","5","3","5",],
+        ["[['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]",
+         "Jugador1",
+        "r1:",
+        "c1:",
+        "r2:",
+        "c2:",
+        "['a', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['a', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]",
+        "Par",
+        "[['a', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['a', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]",
+        "Pares Jugador1: 1"
+         ],
         '''Debe salir:
-        Error en tipo de cliente
+        otra cosa
         '''
-    ),
-    (
-    ["i7", "R", "10"],
-    ["Tipo de laptop i5, i7, i9: ",
-     "Tipo de cliente F, R, N: ",
-      "Cantidad de laptops: ",
-    "Total sin dcto: 95,000",
-    "Descuento: 23,750",
-    "Total a pagar: 71,250"],
-    '''Debe salir:
-    Total sin dcto: 95,000
-    Descuento: 23,750
-    Total a pagar: 71,250
-    '''
-    ),
-    (
-    ["i7", "f", "10"],
-    ["Tipo de laptop i5, i7, i9: ",
-    "Tipo de cliente F, R, N: ",
-    "Cantidad de laptops: ",
-    "Total sin dcto: 95,000",
-    "Descuento: 28,500",
-    "Total a pagar: 66,500"],
-    '''Debe salir:
-    Total sin dcto: 95,000
-    Descuento: 28,500
-    Total a pagar: 66,500
-    '''
-    ),
-
-    (
-    ["i7", "n", "10"],
-    ["Tipo de laptop i5, i7, i9: ",
-    "Tipo de cliente F, R, N: ",
-    "Cantidad de laptops: ",
-    "Total sin dcto: 95,000",
-    "Descuento: 9,500",
-    "Total a pagar: 85,500"
-    ],
-    '''Debe salir:
-    Total sin dcto: 95,000
-    Descuento: 9,500
-    Total a pagar: 85,500
-    '''
-    ),
-
-    (
-    ["i7", "R", "10"],
-    ["Tipo de laptop i5, i7, i9: ",
-    "Tipo de cliente F, R, N: ",
-    "Cantidad de laptops: ",
-    "Total sin dcto: 95,000",
-    "Descuento: 23,750",
-    "Total a pagar: 71,250"
-    ],
-    '''Debe salir:
-    Total sin dcto: 95,000
-    Descuento: 23,750
-    Total a pagar: 71,250
-    '''
-    ),
-
-    (
-    ["i5", "f", "10"],
-    ["Tipo de laptop i5, i7, i9: ",
-    "Tipo de cliente F, R, N: ",
-    "Cantidad de laptops: ",
-    "Total sin dcto: 75,000",
-    "Descuento: 22,500",
-    "Total a pagar: 52,500"
-
-    ],
-    '''Debe salir:
-    Total sin dcto: 75,000
-    Descuento: 22,500
-    Total a pagar: 52,500
-    '''
-    ),
-    (
-    ["i9", "f", "19"],
-    ["Tipo de laptop i5, i7, i9: ",
-    "Tipo de cliente F, R, N: ",
-    "Cantidad de laptops: ",
-    "Total sin dcto: 218,500",
-    "Descuento: 65,550",
-    "Total a pagar: 152,950"
-    ],
-    '''Debe salir:
-    Total sin dcto: 218,500
-    Descuento: 65,550
-    Total a pagar: 152,950
-    '''
-    ),
-    (
-    ["i9", "n", "10"],
-    ["Tipo de laptop i5, i7, i9: ",
-    "Tipo de cliente F, R, N: ",
-    "Cantidad de laptops: ",
-    "Total sin dcto: 115,000",
-    "Descuento: 11,500",
-    "Total a pagar: 103,500"
-    ],
-    '''Debe salir:
-    Total sin dcto: 115,000
-    Descuento: 11,500
-    Total a pagar: 103,500
-    '''
-    ),
-
-    (
-    ["i9", "r", "10"],
-    ["Tipo de laptop i5, i7, i9: ",
-    "Tipo de cliente F, R, N: ",
-    "Cantidad de laptops: ",
-    "Total sin dcto: 115,000",
-    "Descuento: 28,750",
-    "Total a pagar: 86,250"
-    ],
-    '''Debe salir:
-    Total sin dcto: 115,000
-    Descuento: 28,750
-    Total a pagar: 86,250
-
-    '''
     )
+
 
 ]
