76dadcbfeabc84ca8ff21f6608c578ae9b6e6c2b
roque63 <roque@itesm.mx>
2023-04-17 19:07:16 -0600

    actualizacion exercise

--- assignments/04TiendaSillas/src/exercise.py
+++ assignments/04TiendaSillas/src/exercise.py
@@ -3,12 +3,26 @@
 # Carrera -
 figura = "?"
 def leer_cartas_jugador(nombre, tablero):
+    # desplegar el nombre del jugador recibido en el parámetro nombre
+    # leer la carta1 r1, c1  " r1":, "c1:"
+    # validar que la carta este en el rango 0..5 y que esa carta no  este visible
+    # leer la carta2 r2, c2  " r2":, "c2:"
+    # validar que la carta este en el rango 0..5 y que esa carta no  este visible
+    # y que sea diferente de la carta1
     pass
 
 def  despliega_tablero(opcion, matriz, r1 = None, c1 = None, r2 = None, c2 = None):
     pass
 
 def son_pares(opcion, tablero, escondidas, r1, c1, r2, c2):
+    # destapar las cartas en el tablero
+    # desplegar_tablero resaltando las cartas seleccionadas
+    # verificar si son pares
+    # si si son pares asignar a la variable puntos = 1
+    #     desplegar "Par"
+    # si no son pares asignar a  puntos = 0
+    #     desplegar "Impar"
+    #     volver a tapar las cartas en el tablero
     pass
 
 def main() :
